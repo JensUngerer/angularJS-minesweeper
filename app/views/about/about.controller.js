b73249e818a41af7f26ea3767b8198d786412c6d
Jens Ungerer <github@jens-ungerer.de>
2019-04-30 07:11:52 +0200

    Added JSDoc to view controllers.

--- app/views/about/about.controller.js
+++ app/views/about/about.controller.js
@@ -5,10 +5,12 @@
   .module('minesweeperApp')
   .controller('AboutController', AboutController);
 
+  /**
+   * An AngularJS-Controller which is bound in the index.js to the $routeProvider for the about-view.
+   */
   function AboutController() {
     "ngInject";
 
     var vm = this;
   }
-
 })();
--- app/views/home/home.controller.js
+++ app/views/home/home.controller.js
@@ -5,11 +5,11 @@
     .module('minesweeperApp')
     .controller('HomeController', HomeController);
 
+  /**
+   * An AngularJS-Controller which is bound in the index.js to the $routeProvider for the home-view.
+   */
   function HomeController() {
     "ngInject";
     var vm = this;
-
-    // vm.message = "Welcome to the home page";
   }
-
 })();
