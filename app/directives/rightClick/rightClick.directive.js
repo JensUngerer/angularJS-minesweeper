f174705b1cd4597089a0af52b5c47ef119730f0d
Jens Ungerer <github@jens-ungerer.de>
2019-04-30 07:36:52 +0200

    Added JSDoc to rightClick.directive.js.

--- app/directives/rightClick/rightClick.directive.js
+++ app/directives/rightClick/rightClick.directive.js
@@ -1,13 +1,17 @@
 (function () {
     "use strict";
-
-    //cf: http://stackoverflow.com/questions/15731634/how-do-i-handle-right-click-events-in-angular-js
-    //cf: http://jsfiddle.net/b0sx1ae1/
-
+    
     angular.module('minesweeperApp').directive('rightClick', rightClick);
 
     rightClick.$inject = ['$parse'];
 
+    /**
+     * //cf: http://stackoverflow.com/questions/15731634/how-do-i-handle-right-click-events-in-angular-js
+     * //cf: http://jsfiddle.net/b0sx1ae1/
+     *
+     * @param {*} $parse
+     * @returns
+     */
     function rightClick($parse) {
         return function (scope, element, attrs) {
             var fn = $parse(attrs.rightClick);
